32ea4d2c2ff3476cfeca8f2925bfbe5ebf1e05b1
Salome Naskidashvili <send.to.salome@gmail.com>
2022-06-27 19:57:23 +0400

    styles and notifications

--- submit.php
+++ submit.php
@@ -3,19 +3,26 @@ $host = "localhost";
 $db = "demos";
 $username = "root";
 $password= "root";
-$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
-try{
-    $conn = new PDO ($dsn, $username, $password);
+try 
+{
+    $conn = new PDO("mysql:host=$host;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch(PDOException $e){
-    echo 'Connection failed: ' . $e->getMessage();
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
 }
-$response = array ('success'=> false);
-if(isset($_POST['usname']) && $_POST['usname']!=''&& isset($_POST['surname']) && $_POST['surname']!='' &&isset($_POST['email']) && $_POST['email']!=''){
-    $sql = "INSERT INTO contacts(usname, surname, email) VALUES ('".addslashes($_POST['usname'])."', '".addslashes($_POST['surname'])."', '".addslashes($_POST['email'])."')";
-    if($conn->query($sql)){
-        $response['success'] = true;
-    }
+
+$response = array('success' => false);
+
+if(isset($_POST['usname']) && $_POST['usname']!='' && isset($_POST['surname']) && $_POST['surname']!='' && isset($_POST['email']) && $_POST['email']!='')
+{
+	$sql = "INSERT INTO contacts(usname, surname, email) VALUES('".addslashes($_POST['usname'])."', '".addslashes($_POST['surname'])."', '".addslashes($_POST['email'])."')";
+	
+	if($conn->query($sql))
+	{
+		$response['success'] = true;
+	}
 }
+
 echo json_encode($response);
